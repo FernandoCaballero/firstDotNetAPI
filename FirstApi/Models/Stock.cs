a39fc15e2fc2ba8e7d0b9572eed33c77a67080de
FernandoCaballero <fernandocaballero966@gmail.com>
2026-02-24 17:48:58 +0100

    Primeras clases

---
+++ FirstApi/Models/Stock.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")] //El campo solo puede tener 18 dígitos y 2 decimales
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDividend { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
