f10e574ecb0b7cd130133fe60d1a26dbbc7e88d0
FernandoCaballero <fernandocaballero966@gmail.com>
2026-03-03 15:40:59 +0100

    Igual que el anterior commit

--- FirstApi/obj/Debug/net8.0/FirstApi.AssemblyInfo.cs
+++ FirstApi/obj/Debug/net8.0/FirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d9296b35c26768e0c232dd87045d805d38bec4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f58960393270b01ecfb31b64ce79c9fcb13ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
