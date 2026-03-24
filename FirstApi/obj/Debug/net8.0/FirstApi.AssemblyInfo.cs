c48daa659715e6cf42ffb92d6de43f5da8d31634
FernandoCaballero <fernandocaballero966@gmail.com>
2026-03-24 12:35:05 +0100

    Cambios irrelevantes

--- FirstApi/obj/Debug/net8.0/FirstApi.AssemblyInfo.cs
+++ FirstApi/obj/Debug/net8.0/FirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec820f621d8cc7afca0dc35f93707c8d4dc5c88d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b40e0d247efedb7f442b98df162df596b7a0576")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
